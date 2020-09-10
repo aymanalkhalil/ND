ab662de77b12abd55187c51bb258aec4e2c2b4a1
Ayman Tariq <aymanalkhalil12@gmail.com>
2020-09-11 03:29:30 +0400

    make change

--- logout.php
+++ logout.php
@@ -2,17 +2,17 @@
 session_start();
 switch (key($_GET)) {
     case 'u':
-        unset($_SESSION['user_id'], $_SESSION['user_name'], $_SESSION['active']);
+        unset($_SESSION['user_id'], $_SESSION['user_name']);
         header('location:index.php');
         break;
     case 's';
-        unset($_SESSION['sponsor_id'], $_SESSION['sponsor_name'], $_SESSION['active']);
+        unset($_SESSION['sponsor_id'], $_SESSION['sponsor_name']);
         header('location:index.php');
 
         break;
 
     case 'm';
-        unset($_SESSION['merchant_id'], $_SESSION['merchant_name'], $_SESSION['active']);
+        unset($_SESSION['merchant_id'], $_SESSION['merchant_name']);
         header('location:index.php');
 
         break;
