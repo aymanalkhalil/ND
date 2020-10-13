6b5aee3e640a06bbf64af7a07a4a746ff7f58ab5
Ayman Tariq <aymanalkhalil12@gmail.com>
2020-10-13 18:54:24 +0400

    real version

--- admin/includes/connection.php
+++ admin/includes/connection.php
@@ -8,3 +8,7 @@ if (!$conn) {
 
     echo "Cannot Connect to Database " . mysqli_error($conn);
 }
+//mysqli_query($conn, "SET NAMES 'utf8'");
+// mysqli_query($conn, "SET CHARACTER SET UTF8");
+//mysql_query($conn,"set character_set_server='UTF8'");
+mysqli_set_charset($conn, "utf8");
--- includes/connection.php
+++ includes/connection.php
@@ -6,4 +6,7 @@ $conn = mysqli_connect('localhost', 'root', 'root', 'national_day');
 if (!$conn) {
 
     echo "Cannot Connect to Database " . mysqli_error($conn);
-}
+}//mysqli_query($conn, "SET NAMES 'utf8'");
+// mysqli_query($conn, "SET CHARACTER SET UTF8");
+//mysql_query($conn,"set character_set_server='UTF8'");
+mysqli_set_charset($conn, "utf8");
